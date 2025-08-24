e567fc3015faed9dc280fc62f77526bf069aa59a
Pavan-AD76 <pavan.aids.076@gmail.com>
2025-08-24 13:14:43 +0530

    Update Scraping_remoteok.py

--- Scraping_remoteok.py
+++ Scraping_remoteok.py
@@ -72,21 +72,22 @@ print(f"Scraping completed. {len(df)} jobs saved to remoteok_jobs.csv")
 driver.quit()'''
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import tempfile
 
 options = Options()
-options.add_argument("--headless") 
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-software-rasterizer")
 options.add_argument("--remote-debugging-port=9222")
 
-import tempfile
-temp_dir = tempfile.mkdtemp()
-options.add_argument(f"--user-data-dir={temp_dir}")
+options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
 from selenium import webdriver
@@ -210,6 +211,7 @@ df.to_csv("remoteok_jobs_output.csv", index=False)
 print(f"Scraping completed. {len(df)} jobs saved to remoteok_jobs.csv")
 
 driver.quit()
+
 
 
 
